c5a4af46c403e756f19627dc49a15ca2fb8b4a26
shyam <shyamsundar.shanmugam@wipro.com>
2015-05-12 23:21:59 +0530

    showing description and implication in popovers

--- app/main.js
+++ app/main.js
@@ -16,7 +16,21 @@ require.config({
 require(['jquery', 'bootstrap', './questions', 'text!templates/question-group.hbs', 'handlebars'], function($, bootstrap, questionGroups, tmpl, Handlebars){
   $('header h1').html('Single Page Applications');
   var template = Handlebars.compile(tmpl);
+  var nameToId = function(name) {
+    return name.replace(/\W+/g, '-').toLowerCase();
+  };
+  var qId = 0;
   questionGroups.forEach(function(question) {
+    question.id = nameToId(question.name);
+    question.children.forEach(function(child) {
+      child.id = nameToId(child.name);
+      if ( child.questions != undefined ) {
+        child.questions.forEach(function(q) {
+          q.id = 'q-' + qId++;
+        });
+      }
+    });
     $('#questions').append(template(question));
   });
+  $('[data-toggle="popover"]').popover({html: true, container: 'body'})
 })
