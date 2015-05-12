f0add5a44307a2a5e3b5437d9a3449dd2acba7f3
shyam <shyamsundar.shanmugam@wipro.com>
2015-05-13 02:16:34 +0530

    displaying radar chart

--- app/main.js
+++ app/main.js
@@ -3,34 +3,160 @@ require.config({
   shim : {
     bootstrap : {
       deps : ['jquery']
+    },
+    'radar-chart': {
+      deps: ['d3'],
+      exports: 'RadarChart'
     }
   },
   paths: {
     jquery: 'vendor/jquery/dist/jquery',
     bootstrap: 'vendor/bootstrap/dist/js/bootstrap',
     handlebars: 'vendor/handlebars/handlebars',
-    text: 'vendor/text/text'
+    text: 'vendor/text/text',
+    d3: 'vendor/d3/d3',
+    'radar-chart': 'vendor/radar-chart-d3/src/radar-chart'
   }
 });
 
-require(['jquery', 'bootstrap', './questions', 'text!templates/question-group.hbs', 'handlebars'], function($, bootstrap, questionGroups, tmpl, Handlebars){
+require(['d3', 'radar-chart'], function(d3, RadarChart) {
+  var s = 400, w = s, h = s;
+  var colorscale = d3.scale.category10();
+  var fwkKeys = [
+    'angular-js',
+    'backbone-js',
+    'ember-js',
+    'knockout-js',
+    'meteor-js',
+    'ext-js'
+  ];
+  //Legend titles
+  var LegendOptions = ['AngularJS','BackboneJS', 'EmberJS', 'KnockoutJS', 'Meteor', 'ExtJS'];
+
+  d3.csv('app/csv/fwk-params.csv')
+    .get(function(err, rows) {
+      if ( err ) throw err;
+
+      var fwks = {
+        'angular-js': [],
+        'backbone-js': [],
+        'ember-js': [],
+        'knockout-js': [],
+        'meteor-js': [],
+        'ext-js': []
+      };
+      rows.forEach(function(row) {
+        fwkKeys.forEach(function(fwk) {
+          fwks[fwk].push({axis: row.parameter, value: row[fwk] });
+        })
+      });
+      var radarData = fwkKeys.map(function(fwk) {
+        return fwks[fwk];
+      });
+      drawChart(radarData);
+    });
+
+  function drawLegend() {
+    var svg = d3.select('#right')
+        .selectAll('svg')
+        .append('svg')
+        .attr("width", w+300)
+        .attr("height", h)
+
+    //Create the title for the legend
+    var text = svg.append("text")
+        .attr("class", "title")
+        .attr('transform', 'translate(90,0)')
+        .attr("x", w - 70)
+        .attr("y", 10)
+        .attr("font-size", "12px")
+        .attr("fill", "#404040")
+        .text("SPA Frameworks");
+
+    //Initiate Legend
+    var legend = svg.append("g")
+        .attr("class", "legend")
+        .attr("height", 100)
+        .attr("width", 200)
+        .attr('transform', 'translate(90,20)')
+    ;
+    //Create colour squares
+    legend.selectAll('rect')
+      .data(LegendOptions)
+      .enter()
+      .append("rect")
+      .attr("x", w - 65)
+      .attr("y", function(d, i){ return i * 20;})
+      .attr("width", 10)
+      .attr("height", 10)
+      .style("fill", function(d, i){ return colorscale(i);})
+    ;
+    //Create text next to squares
+    legend.selectAll('text')
+      .data(LegendOptions)
+      .enter()
+      .append("text")
+      .attr("x", w - 52)
+      .attr("y", function(d, i){ return i * 20 + 9;})
+      .attr("font-size", "11px")
+      .attr("fill", "#737373")
+      .text(function(d) { return d; });
+  }
+  function drawChart(data) {
+    //Options for the Radar chart, other than default
+    var radarCfg = {
+      w: w,
+      h: h,
+      maxValue: 1,
+      levels: 10,
+      ExtraWidthX: 300
+    }
+    //Call function to draw the Radar chart
+    //Will expect that data is in %'s
+    RadarChart.draw("#right", data, radarCfg);
+    drawLegend();
+    var svg = d3.select('#right')
+        .select('svg').attr('width', w+300);
+
+  }
+});
+require(['jquery', 'bootstrap', 'handlebars', 'text!templates/fwk-params.hbs'], function($, bootstrap, Handlebars, fwkParamsTemplate){
   $('header h1').html('Single Page Applications');
-  var template = Handlebars.compile(tmpl);
+  var template = Handlebars.compile(fwkParamsTemplate);
   var nameToId = function(name) {
     return name.replace(/\W+/g, '-').toLowerCase();
   };
   var qId = 0;
-  questionGroups.forEach(function(question) {
-    question.id = nameToId(question.name);
-    question.children.forEach(function(child) {
-      child.id = nameToId(child.name);
-      if ( child.questions != undefined ) {
-        child.questions.forEach(function(q) {
-          q.id = 'q-' + qId++;
-        });
-      }
+  console.log(d3);
+  d3.csv('app/csv/fwk-params.csv')
+    .get(function(err, rows) {
+      if ( err ) throw err;
+      var left = $('#left');
+      processRows(rows).forEach(function(row) {
+        left.append(template(row));
+      });
+      $('.weight').on('change', function(){
+        var id = this.id, val = this.value;
+        $('#w-' + id + ' span.pull-right').html(val);
+      });
     });
-    $('#questions').append(template(question));
-  });
-  $('[data-toggle="popover"]').popover({html: true, container: 'body'})
+  function processRows(rows) {
+    var m = {};
+    var ro = [];
+    rows.forEach(function(row) {
+      ro.push(row.group);
+      var a = m[row.group] || [];
+      row.id = nameToId(row.parameter);
+      a.push(row);
+      m[row.group] = a;
+    });
+    ro = ro.sort().filter(function(g, i, a) { return i == 0 ? true : a[i-1] != g; });
+    return ro.map(function(g) {
+      return {
+        id: nameToId(g),
+        name: g,
+        children: m[g]
+      };
+    });
+  }
 })
