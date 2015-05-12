0fa9aaa157a0b171987bbbee6c30b65c971ed0ea
shyam <shyamsundar.shanmugam@wipro.com>
2015-05-12 16:59:13 +0530

    editing questions and capitalizing headings

--- app/questions.js
+++ app/questions.js
@@ -4,17 +4,17 @@ define(function() {
       "name": "Client/User Environment",
       "children": [
         {
-          "name": "Interent vs. Intranet (Uncontrolled vs. Controlled | Trusted vs Untrusted)",
+          "name": "Interent vs. Intranet",
           "explanation": "Intranet applications are used by a smaller group of users typically within a company (sometimes including vendors) distributed in geographical clusters. Intranet applications mostly will only be accessible from within an office network. Internet applications cater to a much wider audience which may be distributed.",
           "implication": "Intranet applications cater to a captive audience whose day-to-day operation requires interaction with the application and hence will be more patient and tolerant of poor performance and experience. The internet users have a shorter attention span and hence the performance and experience needs to be fast and slick. Applications hosted on the internet are more suceptible to be target for malicious attacks and hence need to be protected more rigorously."
         },
         {
-          "name": "Channel Coverage (Desktop, Smartphone, Tablets)",
+          "name": "Channel Coverage",
           "explanation": "What are the primary channels that will be used to access the application.",
           "implication": "Decides what should be the focus mobile-first vs. web-first. Influences all aspects of the application from UI, UX and application design. Performance characteristics for handheld devices will be lower compared to desktops/laptops."
         },
         {
-          "name": "Connectivity (Speed of network connectivity)",
+          "name": "Connectivity",
           "explanation": "Connectivity of the application to the device that will be using the application. Desktops/laptops might connect through LAN/WiFi but, handheld devices might connect through WiFi or low-bandwidth EDGE/2G/3G connections",
           "implication": "Connectivity determines the performance of the application. Applications that have to operate in poor connectivity environments can benefit from an offline first apporach to development. Testing the application in low-bandwidth connections can yield critical insights to optimizing application performance"
         }
@@ -24,7 +24,7 @@ define(function() {
       "name": "User Interface",
       "children": [
         {
-          "name": "Standard UI elements vs. Custom UI elements",
+          "name": "Standard vs. Custom UI elements",
           "explanation": "HTML offers a standard set of form elements like the textbox, selectbox, file upload and textarea. These might not suit the needs for various data formats. Eg. Dates, credit cards etc.",
           "implication": "Traditional UI elements offered by HTML do not provide a rich user experiene. Widget and component libraries can augment the existing set of elements to provide a richer experience. But, such components can be difficult to integrate and might not play well with other libraries."
         },
@@ -44,7 +44,7 @@ define(function() {
           "implication": "Moving away from a form based validation approach will lead to primarily model validation which can be more consistent across the client and the server. Similar to ORMs on the server-side OIMs (Object Interface Mappers) can be used to generate the templates and model validations both on the client and server."
         },
         {
-          "name": "Same UI metaphor across channels vs. Mobile First",
+          "name": "UI metaphors",
           "explanation": "MVC is a common metaphor that is available both on the client-side and server-side development",
           "implication": "There are several flavors of MV*"
         }
@@ -54,17 +54,17 @@ define(function() {
       "name": "Server Environment",
       "children": [
         {
-          "name": "Green field vs. Brown field",
+          "name": "Green vs. Brown field",
           "explanation": "If there is an existing application that will be modernized. Then reuse of atleast some of it components becomes an important factor in choosing a framework.",
           "implication": "When resuing existing components there might be some additional effort required to refactor/generate the target code. In case of some frameworks like GWT which is in itself a code-generation based framework very little of the application code can be reused."
         },
         {
-          "name": "Service Interfaces (XML vs JSON, HTTP methods)",
+          "name": "Service Interfaces",
           "explanation": "In case of existing applications there might already exist web service endpoints which can be utilized by the modernization. In new development most frameworks tend to work well with JSON based REST services.",
           "implication": "When existing interfaces are reused we might be incurring some technical debt because of the dated interfaces which may need replacement for future development."
         },
         {
-          "name": "Isomorphic capability (Capability exists to execute code on server when needed, reuse models/validations)",
+          "name": "Isomorphism",
           "explanation": "Isomorphism is the ability to run the same code on the client and the server. Since the introduction of NodeJS in 2009 isomorphic javascript applications have become a reality.",
           "implication": "Isomorphism allows better reuse of some aspects of the application. But, as a concept it is fairly new and most frameworks in this area are in their nascent stages of development. Isomorphic frameworks are very suited to build realtime interactive applications."
         }
@@ -94,12 +94,12 @@ define(function() {
       "name": "Application Needs",
       "children": [
         {
-          "name": "Size of possible codebase",
+          "name": "Codebase Size",
           "explanation": "The estimated size of the application in terms of the number of screens and the possible functionalities can be a good indicator of the complexity of the application. The more complex the application the more it needs to be simplified in terms of breaking up of functionality into smaller components.",
           "implication": "If the choice of framework imposes a structure on organizing the codebase it is easier for new developers to quickly become productive. Otherwise they might have to spend more time getting familiar with a large codebase."
         },
         {
-          "name": "Complexity of data entities invovled (model richness and attribute types)",
+          "name": "Complexity of entities",
           "explanation": "Every application is unique and the underlying domain models will vary. OOP principles such as abstraction, composition and inheritance have to be used.",
           "implication": "Translating existing server-side models to the client-side might in some cases be counter-productive as the interactions that are possible on the client and server are vastly different. Model design might be dictated by some frameworks but, in cases where it is not careful consideration to the design of the models must be made."
         },
@@ -109,12 +109,12 @@ define(function() {
           "implication": "Templates rendering in SPAs happens on the browser and hence search engine crawlers which cannot evaluate javascript might not be able to index the application. SPAs also use a lot of the browser specific features such as history manipulation and hash urls so, any web crawlers which cannot handle them cannot index the application."
         },
         {
-          "name": "Modularity (functional modules, lazy load modules)",
+          "name": "Modularity",
           "explanation": "Developing and maintaining a large monolithic codebase is cumbersome. So, the ability to break up functionality into modules allows the application to have a coherent structure and streamlines development and maintenance.",
-          "implication": "Decomposition of modules can be done several ways and sometimes frameworks themselves dictate how modules and code should be structured. Dependency management and on-demand loading of modules will be required for large applications to improve performance and memory consumption on the browser."
+          "implication": "Decomposition of modules can be done several ways and sometimes frameworks themselves dictate how modules and code should be structured. Dependency management and on-demand loading (lazy loading) of modules will be required for large applications to improve performance and memory consumption on the browser."
         },
         {
-          "name": "Embeddable / partial usage (portal or SSO)",
+          "name": "Embeddability",
           "explanation": "Applications might be part of a suite of applications or a set of enterpriser intranet applications and such requirements may impose additional constraints on framework selection.",
           "implication": "Since SPA frameworks rely on history and url manipulation they might have some constraints working in a portal environment or running within frames etc."
         },
