48467676ae8d32fc4862a409ec0c6fc46a04f0af
shyam <shyamsundar.shanmugam@wipro.com>
2015-05-12 16:51:25 +0530

    using handlebar templates to display questions

--- app/main.js
+++ app/main.js
@@ -7,10 +7,16 @@ require.config({
   },
   paths: {
     jquery: 'vendor/jquery/dist/jquery',
-    bootstrap: 'vendor/bootstrap/dist/js/bootstrap'
+    bootstrap: 'vendor/bootstrap/dist/js/bootstrap',
+    handlebars: 'vendor/handlebars/handlebars',
+    text: 'vendor/text/text'
   }
 });
 
-require(['jquery', 'bootstrap'], function($, bootstrap){
+require(['jquery', 'bootstrap', './questions', 'text!templates/question-group.hbs', 'handlebars'], function($, bootstrap, questionGroups, tmpl, Handlebars){
   $('header h1').html('Single Page Applications');
+  var template = Handlebars.compile(tmpl);
+  questionGroups.forEach(function(question) {
+    $('#questions').append(template(question));
+  });
 })
