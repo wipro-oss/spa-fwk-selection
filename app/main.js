978e583516c15ea4034deddf93e4db8a68b688be
shyam <shyamsundar.shanmugam@wipro.com>
2015-05-14 11:40:06 +0530

    showing table

--- app/main.js
+++ app/main.js
@@ -135,41 +135,140 @@ require(['d3', 'radar-chart'], function(d3, RadarChart) {
     });
   }
 });
-require(['jquery', 'bootstrap', 'handlebars', 'text!templates/fwk-params.hbs'], function($, bootstrap, Handlebars, fwkParamsTemplate){
+require(['jquery', 'bootstrap', 'handlebars', 'text!templates/fwk-params.hbs', 'text!templates/fwk-table.hbs'], function($, bootstrap, Handlebars, fwkParamsTemplate, fwkTableTemplate){
   $('header h1').html('Single Page Applications');
+  var fwkKeys = [
+    'angular-js',
+    'backbone-js',
+    'ember-js',
+    'knockout-js',
+    'meteor-js',
+    'ext-js'
+  ];
+  var fwkLabels = {
+    'angular-js'  : 'AngularJs',  
+    'backbone-js' : 'BackboneJs',
+    'ember-js'    : 'EmberJs',    
+    'knockout-js' : 'KnockoutJs',
+    'meteor-js'   : 'Meteor',     
+    'ext-js'      : 'ExtJs'
+  };
+  var gidMap = {};
+  var pidMap = {};
   var template = Handlebars.compile(fwkParamsTemplate);
+  var tableTemplate = Handlebars.compile(fwkTableTemplate)
   var nameToId = function(name) {
     return name.replace(/\W+/g, '-').toLowerCase();
   };
   var qId = 0;
-  console.log(d3);
   d3.csv('app/csv/fwk-params.csv')
     .get(function(err, rows) {
       if ( err ) throw err;
-      var left = $('#left');
-      processRows(rows).forEach(function(row) {
-        left.append(template(row));
+      var model = processRows(rows);
+      updateModel();
+      console.log(model);
+      console.log(tableModel());
+      $('#table').html(tableTemplate(tableModel()));
+      var paramGroup = $('#param-groups');
+      model.forEach(function(row) {
+        paramGroup.append(template(row));
       });
+      function updateModel(gid, id, val) {
+        model.forEach(function(group) {
+          if ( gid === group.id ) {
+            group.children.forEach(function(param) {
+              if ( id === param.id ) {
+                param.weight = parseFloat(val);
+              }
+            });
+          }
+          var n = 0, s = 0;
+          fwkKeys.forEach(function(fwk) {
+            group[fwk] = 0;
+          });
+          group.children.forEach(function(param) {
+            if ( param.weight != 0 ) {
+              n++;
+              s += param.weight;
+            }
+            var pvMap = group.parameters[param.id];
+            fwkKeys.forEach(function(fwk) {
+              group[fwk] = group[fwk] || 0;
+              group[fwk] += (param.weight * pvMap[fwk]);
+            })
+          });
+          group.total = (s/n).toFixed(2);
+        });
+      }
+      function tableModel() {
+        var header = ['Parameter'].concat(fwkKeys.map(function(fwk) { return fwkLabels[fwk]; }));
+        var rows = model.map(function(group) {
+          return [group.name].concat(fwkKeys.map(function(fwk) { return group[fwk]; }));
+        });
+        return {
+          header: header,
+          rows: rows
+        };
+      }
       $('.weight').on('change', function(){
         var id = this.id, val = this.value;
+        /*
         $('#w-' + id + ' span.pull-right').html(val);
+        var panel = $(this).closest('.panel');
+        var n = 0, s = 0;
+        panel.find('input').each(function(i, v) {
+          if ( v.value !== '0' ) {
+            n++;
+            s += parseFloat(v.value);
+          }
+        });
+        panel.find('.badge').html((s/n).toFixed(2));
+        */
+        updateModel(this.dataset['group'], id, val);
+        console.log(model);
+        $('#table').html(tableTemplate(tableModel()));
+        $('#table').html(tableTemplate(tableModel()));
+        var paramGroup = $('#param-groups');
+        $('#param-groups').empty();
+        model.forEach(function(row) {
+          paramGroup.append(template(row));
+        });
       });
     });
   function processRows(rows) {
-    var m = {};
+    var m = {}, t = {}, wm = wm = {};
     var ro = [];
     rows.forEach(function(row) {
       ro.push(row.group);
       var a = m[row.group] || [];
+      var ti = t[row.group] || { n: 0, s: 0};
       row.id = nameToId(row.parameter);
-      a.push(row);
+      pidMap[row.id] = row.parameter;
+      a.push({ id: row.id, parameter: row.parameter, group: row.group, weight: parseFloat(row.weight) });
       m[row.group] = a;
+      if ( row.weight != '0' ) {
+        ti.n++;
+        ti.s += parseFloat(row.weight);
+      }
+      t[row.group] = ti;
+      var gid = nameToId(row.group);
+      gidMap[gid] = row.group;
+      var wi = wm[gid] || {};
+      wi[row.id] = {};
+      fwkKeys.forEach(function(fwk) {
+        var v = '' == row[fwk] ? 0 : parseFloat(row[fwk]);
+        wi[row.id][fwk] = v == NaN ? 0 : v;
+      });
+      wm[gid] = wi;
     });
     ro = ro.sort().filter(function(g, i, a) { return i == 0 ? true : a[i-1] != g; });
     return ro.map(function(g) {
+      var gid = nameToId(g);
       return {
-        id: nameToId(g),
+        id: gid,
         name: g,
+        total: (t[g].s / t[g].n).toFixed(2),
+        parameters: wm[gid],
         children: m[g]
       };
     });
